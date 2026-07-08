c59330e4449f29d096e109e2c14f0b3db2abb48b
Camron Hall <slusanga8403@gmail.com>
2026-07-08 14:33:33 -0500

    Home computer

--- syntaxProblems.cpp
+++ syntaxProblems.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-   //Doing syntax problems
+   //Ask for two integers and print their sum
+
+   int x, y;
    
-   cout << "Hello, C++!\n";
+   cout << "Enter two positive integers:\n";
+   cin >> x;
+   cin >> y;
+   cout << "Now display their sum:\n";
+   
+   int sum = x + y;
+
+   cout << sum;
+
+   
    
    return 0;
 }
